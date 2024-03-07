ea7a8cf0726f35c6257fda0f0bdac9c50de9d99f
Tronciu Dumitru <72997097+Parsyfall@users.noreply.github.com>
2024-03-07 18:02:54 +0200

    First implementation (#1)
    * feat: add implementation for mutation function
    	chore: add .gitignore
    
    * feat: implement fitness function
    fix: typos
    
    * feat(mutation): now operates on a entire population instead of a
    chromosome
    feat(fitness): check each neighbour of a cell
    
    * feat(fiteness): changed returned value
    	Return a value representing how close is a given chromosome to
    perfect fold
    
    * feat: refactor chromosome into a class
    
    * feat: mov Chromosome class to a separate file
    
    * style(chromosome.py): move constructor to the top of class
    main.py
    	fix(singlePointCrossover): correct return value, now really returns a chromosome tuple
    	feat(mutation): add a probability check
    	refactors(mutation): minor implementation change
    
    * feat: implement selection operator
     refactor: change imports
    
    * feat: add implementation for run_evolution function
    refactor: change some function and argument names

---
+++ chromosome.py
@@ -0,0 +1,69 @@
+from typing import List
+import numpy as np
+from random import choices
+
+
+class Chromosome:
+    sequence: List[str] = []
+    fitness: float = 0.0
+
+    def __init__(self, length: int = 1, sequence: List[str] = []) -> None:
+        if not sequence:
+            sequence = self.generate_chromosome(length)
+        self.sequence = sequence
+        self.fitness = self.calc_fitness()
+
+    @staticmethod
+    def get_lattice(lines: int, columns: int) -> np.ndarray:
+        return np.full((lines, columns), -1, dtype=np.int8)
+
+    @staticmethod
+    def generate_chromosome(length: int) -> List[str]:
+        return choices(["R", "U", "L", "D"], k=length)
+
+    def calc_fitness(self) -> float:
+        # TODO: Try orthogonal axes and a vector of points instead of matrix
+        length = len(self.sequence)
+        lattice = self.get_lattice(length, length)
+
+        collision = score = 0
+        X = Y = length - 1
+
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        flag = False
+
+        for letter in self.sequence:
+            if lattice[X][Y] in [0, 1]:
+                collision += 1
+
+            lattice[X][Y] = 0 if letter == "H" else 1
+
+            if flag and letter == "H":
+                score -= 1
+
+            for dir in directions:
+                if 0 <= X + dir[0] < length and 0 <= Y + dir[1] < length:
+                    if lattice[X + dir[0]][Y + dir[1]] == 0:
+                        score += 1
+
+            match letter:
+                case "U":
+                    X -= 1
+                case "D":
+                    X += 1
+                case "R":
+                    Y += 1
+                case "L":
+                    Y -= 1
+
+            flag = letter == "H"
+
+        if collision == 0:
+            self.fitness = score * 100 + 1
+        elif collision == 2:
+            self.fitness = (score * 100 + 1) / 2
+        else:
+            self.fitness = (score * 100 + 1) / collision * collision
+
+        return self.fitness
--- main.py
+++ main.py
@@ -1,48 +1,97 @@
-from random import choices, randint
+import random as rd
+from chromosome import Chromosome
 
-# Initial sequence of amino acids, "HPPHPPH" or "01001010"
-Sequence = list[int]  # or list[str]
-
-# A set o rules (RULD) representing a self-avoiding path in a lattice
-Chromosome = list[str]
 
 Population = list[Chromosome]
 
 
-def generateChromosome(length: int) -> Chromosome:
-    return choices(["R", "U", "L", "D"], k=length)
-
-
 # Initialization: generate initial generation
-def generatePopulation(size: int, chromosome_length: int) -> Population:
-    return [generateChromosome(chromosome_length) for _ in range(size)]
+def generate_population(population_size: int, chromosome_length: int) -> Population:
+    return [
+        Chromosome.generate_chromosome(chromosome_length)
+        for _ in range(population_size)
+    ]
 
 
-# Evaluate a chromosome fittnes
-def fitness(chromosome: Chromosome):
-    return NotImplementedError
+# Performe crossover on two parents and return two new children
+def single_point_crossover(
+    a: Chromosome, b: Chromosome
+) -> tuple[Chromosome, Chromosome]:
+    if len(a.sequence) != len(b.sequence):
+        raise ValueError("Genome a and b must be of the same length")
 
+    lenght = len(a.sequence)
 
-# Performe crossover on two parents and return two new childs
-def singelPointCrossover(a: Chromosome, b: Chromosome) -> tuple[Chromosome, Chromosome]:
-    if len(a) != len(b):
-        raise ValueError("Genome a and b must be of the same lenght")
-    
-    lenght = len(a)
-    
     if lenght < 2:
         return a, b
 
-    p = randint(1, lenght -1)
-    return a[0:p] + b[:p], b[0:p] + a[:p]
-
+    p = rd.randint(1, lenght - 1)
+    return (
+        Chromosome(a.sequence[0:p] + b.sequence[:p]),
+        Chromosome(b.sequence[0:p] + a.sequence[:p]),
+    )
 
 
 # Performe a mutation on a specific Chromosome
-def mutation():
-    return NotImplementedError
+def mutation(population: Population, probability: float):
+    if not (0 <= probability <= 1):
+        raise ValueError("Probability must be within the interval [0, 1]")
+
+    for chromosome in population:
+        if rd.random() <= probability:
+            index = rd.randrange(len(chromosome.sequence))
+            if chromosome.sequence[index] in ["R", "L"]:
+                options = ["R", "U", "D"]  # Removing "L" as it's antipod move
+            else:
+                options = ["L", "U", "D"]  # Removing "R" as it's antipod move
+
+            chromosome.sequence[index] = rd.choice(options)
 
 
 # Selection: prepare the next generation
-def selection():
-    return NotImplementedError
+def tournament_selection(
+    population: Population, tournament_size: int, offsprings_number: int
+) -> Population:
+    Temp: Population = []
+    Best: Population = []
+    for _ in range(offsprings_number):
+        # TODO: Find something faster/more efficient than a list for Temp
+        Temp = rd.choices(population, k=tournament_size)
+        Best.append(tournament(Temp))
+
+    return Best
+
+
+def tournament(participants: list[Chromosome]) -> Chromosome:
+    Best: Chromosome = participants[0]
+    for participant in participants[1:]:
+        if participant.fitness > Best.fitness:
+            Best = participant
+
+    return Best
+
+
+def run_evolution(population_size: int, max_generations: int) -> Chromosome:
+    # Initialize population
+    population: Population = generate_population(population_size, 10)
+
+    generation_number: int = 0
+    while generation_number < max_generations:
+        population = sorted(population, key=lambda x: x.score, reverse=True)
+        new_population: Population = population[:2]
+
+        for _ in range(population_size - 2):
+            parents = rd.choices(population, k=2)
+            children = single_point_crossover(parents[0], parents[1])
+            new_population.extend(children)
+            
+        tournament_selection(population, 3, population_size - 2)
+        mutation(new_population, 1 / population_size)
+
+        # Recalculate fitness
+        for genome in new_population:
+            genome.calc_fitness()
+
+        population = new_population
+
+    return population[0]
